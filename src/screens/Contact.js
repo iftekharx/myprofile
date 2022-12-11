d9ae4470a36ca2e36edfc1961b2fb5e37c0da868
iftekharx <iftekharx86@gmail.com>
2022-12-11 06:25:11 +0600

    fixed cont

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -21,16 +21,12 @@ function Contact() {
             >
               iftekhar814@gmail.com <br />
             </a>
-            Mobile: 01784208883 <br />
             LinkedIn:{' '}
             <a href="https://www.linkedin.com/in/iftekhar-hyder-34b7b9228/">
               {' '}
               <br />
               https://www.linkedin.com/in/iftekhar-hyder-34b7b9228/ <br />
             </a>
-            Address:Flat A5, Oriental De Square,
-            <br /> House#4, Mymensingh Road, <br />
-            Banglamotor, Paribagh, Dhaka-1000, Bangladesh
           </p>
         </Container>
       </section>
